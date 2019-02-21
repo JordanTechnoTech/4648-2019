8011b5b01e3b8c0fa2e9bd6f737266d14a49c718
Anthony Quinones <quinoant@gmail.com>
2019-02-21 16:06:45 -0600

    splitting commands

--- src/main/java/frc/robot/commands/ElbowCommand.java
+++ src/main/java/frc/robot/commands/ElbowCommand.java
@@ -1,4 +1,25 @@
 package frc.robot.commands;
 
-public class ElbowCommand {
+import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import frc.robot.RobotMap;
+
+public class ElbowCommand extends Command {
+    private double elbowPosition;
+    private boolean finished = false;
+
+    public ElbowCommand(double elbowPosition){
+        this.elbowPosition = elbowPosition;
+        requires(RobotMap.elbowSubsystem);
+    }
+
+    @Override
+    protected void execute() {
+        SmartDashboard.putString("Elbow mode","AUTO");
+        RobotMap.elbowSubsystem.moveElbow(elbowPosition);
+        finished = true;
+    }
+
+    @Override
+    protected boolean isFinished() { return finished;}
 }
--- src/main/java/frc/robot/commands/WristCommand.java
+++ src/main/java/frc/robot/commands/WristCommand.java
@@ -1,4 +1,23 @@
 package frc.robot.commands;
 
-public class WristCommand {
+import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import frc.robot.RobotMap;
+
+public class WristCommand extends Command {
+    private double wristPosition;
+    private boolean finished = false;
+
+    public WristCommand(double wristPosition){
+        this.wristPosition = wristPosition;
+        requires(RobotMap.wristSubsystem);
+    }
+
+    @Override
+    protected void execute() {
+        SmartDashboard.putString("Wrist mode","AUTO");
+    }
+
+    @Override
+    protected boolean isFinished() { return finished;}
 }
