e1e6fb31e765ebaf41703c8df2ad867d2d8bbffa
Anthony Quinones <quinoant@gmail.com>
2019-02-21 15:41:13 -0600

    elbow

--- src/main/java/frc/robot/subsystems/ElbowSubsystem.java
+++ src/main/java/frc/robot/subsystems/ElbowSubsystem.java
@@ -1,7 +1,9 @@
 package frc.robot.subsystems;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.talon.Gains;
 import frc.robot.talon.TalonInitializer;
 
@@ -20,6 +22,21 @@ public class ElbowSubsystem extends Subsystem implements TechnoTechSubsystem  {
 
     @Override
     public void log() {
-
+        SmartDashboard.putNumber("Elbow sensor value", elbow.getSelectedSensorPosition());
+        SmartDashboard.putNumber("Elbow motor output", elbow.getMotorOutputPercent());
     }
+    public void moveElbow(double position) {
+        this.elbow.set(ControlMode.Position, position);
+    }
+    public void moveElbowPower(double power) {
+        this.elbow.set(ControlMode.PercentOutput, power);
+    }
+    public void stopElbow() {
+        this.moveElbow(this.elbow.getSelectedSensorPosition()+600);
+    }
+    public int getElbowPosition() {
+        return this.elbow.getSelectedSensorPosition();
+    }
+
+
 }
