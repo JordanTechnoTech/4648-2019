2e816dc2ee66a6edc4c9b8f490c42acca2049276
Anthony Quinones <quinoant@gmail.com>
2019-02-21 18:27:12 -0600

    improving shoulder

--- src/main/java/frc/robot/commands/ShoulderCommand.java
+++ src/main/java/frc/robot/commands/ShoulderCommand.java
@@ -4,6 +4,7 @@ import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.RobotMap;
 import frc.robot.subsystems.ShoulderSubsystem;
+import frc.robot.talon.Gains;
 
 public class ShoulderCommand extends Command {
     private double shoulderPosition;
@@ -12,13 +13,33 @@ public class ShoulderCommand extends Command {
     public ShoulderCommand(double shoulderPosition){
         this.shoulderPosition = shoulderPosition;
         requires(RobotMap.shoulderSubsystem);
+        SmartDashboard.putData(this);
+        SmartDashboard.putNumber("Shoulder Kp",.75);
+        SmartDashboard.putNumber("Shoulder Ki",0);
+        SmartDashboard.putNumber("Shoulder Kd",1);
+        SmartDashboard.putNumber("Shoulder kF",0);
+        SmartDashboard.putNumber("Shoulder kIzone",0);
+        SmartDashboard.putNumber("Shoulder kPeakOutput",.2);
+        SmartDashboard.putNumber("Shoulder position",100);
     }
 
     @Override
     protected void execute() {
-        SmartDashboard.putString("Shoulder mode", "Auto");
-        RobotMap.shoulderSubsystem.moveShoulder(shoulderPosition);
+        double kp = SmartDashboard.getNumber("Shoulder Kp",.75);
+        double Ki = SmartDashboard.getNumber("Shoulder Ki",0);
+        double Kd = SmartDashboard.getNumber("Shoulder Kd",1);
+        double kF = SmartDashboard.getNumber("Shoulder kF",0);
+        int kIzone = (int) SmartDashboard.getNumber("Shoulder kIzone",0);
+        double kPeakOutput = SmartDashboard.getNumber("Shoulder kPeakOutput",.2);
+        double dbPosition = SmartDashboard.getNumber("Shoulder position",100);
+        Gains gains = new Gains(kp,Ki,Kd,kF,kIzone,kPeakOutput);
+        RobotMap.shoulderSubsystem.moveShoulderToPosition(dbPosition,gains);
         finished = true;
+    }
+
+    @Override
+    public synchronized void cancel() {
+        RobotMap.elbowSubsystem.stopElbow();
     }
 
     @Override
--- src/main/java/frc/robot/subsystems/ShoulderSubsystem.java
+++ src/main/java/frc/robot/subsystems/ShoulderSubsystem.java
@@ -5,15 +5,21 @@ import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import edu.wpi.first.wpilibj.command.Subsystem;
 import edu.wpi.first.wpilibj.smartdashboard.SendableBuilder;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import frc.robot.talon.Constants;
 import frc.robot.talon.Gains;
 import frc.robot.talon.TalonInitializer;
 
 public class ShoulderSubsystem extends Subsystem implements TechnoTechSubsystem {
     private WPI_TalonSRX shoulder;
+    private Gains defaultKGains = new Gains(0.015, 0, 1, 0.0, 0, 1.0);
+
     public ShoulderSubsystem(int shoulderCanId){
         this.shoulder = new WPI_TalonSRX(shoulderCanId);
         this.shoulder.setInverted(true);
-        stopShoulder();
+   //     stopShoulder();
+    }
+    public void resetTalonEncoder() {
+        shoulder.setSelectedSensorPosition(0, Constants.kPIDLoopIdx, Constants.kTimeoutMs);
     }
 
     @Override
@@ -21,11 +27,13 @@ public class ShoulderSubsystem extends Subsystem implements TechnoTechSubsystem 
         SmartDashboard.putNumber("Shoulder sensor value", shoulder.getSelectedSensorPosition());
         SmartDashboard.putNumber("Shoulder motor output", shoulder.getMotorOutputPercent());
     }
-    public void moveShoulder(double position) {
+    public void moveShoulderToPosition(double position, Gains kGains) {
+        TalonInitializer.initTalon(this.shoulder, kGains);
         this.shoulder.set(ControlMode.Position, position);
     }
     public void stopShoulder() {
-        this.moveShoulder(this.shoulder.getSelectedSensorPosition()+600);
+     //   this.moveShoulder(this.shoulder.getSelectedSensorPosition()+600);
+        this.shoulder.stopMotor();
     }
     public int getShoulderPosiion() {
         return this.shoulder.getSelectedSensorPosition();
